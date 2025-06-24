d6f41e69756002619ffd68c79085f767d7d4042a
Siempre <71586429+Naymushin-12@users.noreply.github.com>
2025-06-25 00:16:31 +0500

    Update OfficialHierarchy.h

--- OfficialHierarchy.h
+++ OfficialHierarchy.h
@@ -1,75 +1,46 @@
-#ifndef OFFICIALHIERARCHY_H  // Защита от повторного включения заголовочного файла
+#ifndef OFFICIALHIERARCHY_H
 #define OFFICIALHIERARCHY_H
 
-#include <vector>  
-#include <string>  
+#include <vector>
+#include <string>
+#include <memory>
+#include <unordered_map>
 
-using namespace std;  
-
-
-class OfficialNode { // Класс, представляющий узел чиновника в иерархии
-private:
-    int id;                         // Уникальный идентификатор чиновника
-    int bribeAmount;                // Сумма взятки, требуемая чиновником
-    OfficialNode* boss;             // Указатель на начальника (родительский узел)
-    vector<OfficialNode*> subordinates; // Список подчиненных (дочерние узлы)
-
+class OfficialNode {
 public:
-    // Конструктор узла чиновника
-    OfficialNode(int officialId, int bribe);
+    OfficialNode(int id, int bribe);
     
-    // Установка начальника для текущего чиновника
-    void setBoss(OfficialNode* officialBoss);
-    
-    // Добавление подчиненного к текущему чиновнику
+    void setBoss(OfficialNode* boss);
     void addSubordinate(OfficialNode* subordinate);
     
-    // Получение ID чиновника
     int getId() const;
-    
-    // Получение суммы взятки чиновника
     int getBribe() const;
+    const std::vector<OfficialNode*>& getSubordinates() const;
     
-    // Получение указателя на начальника
-    OfficialNode* getBoss() const;
-    
-    // Получение списка подчиненных (константная ссылка)
-    const vector<OfficialNode*>& getSubordinates() const;
+private:
+    int id;
+    int bribeAmount;
+    std::vector<OfficialNode*> subordinates;
 };
 
-
-
-class OfficialHierarchy { //Класс, представляющий иерархию чиновников
-private:
-    OfficialNode* root;             // Указатель на корневой узел (главный чиновник)
-    vector<OfficialNode*> officials; // Вектор всех чиновников в системе
-
-    // Поиск узла чиновника по ID (приватный метод)
-    OfficialNode* findNodeById(int id) const;
-    
-    // Расчет минимальной суммы взяток (рекурсивный приватный метод)
-    void calculateMinBribe(OfficialNode* node, int& totalBribe, 
-        vector<int>& path) const;
-    
-    // Вывод пути получения подписей (приватный метод)
-    void printPath(const vector<int>& path) const;
-
+class OfficialHierarchy {
 public:
-    int getValidatedInput(const string& prompt, int minVal, int maxVal, bool allowZero);
-    // Конструктор по умолчанию
     OfficialHierarchy();
-    
-    // Деструктор для очистки памяти
     ~OfficialHierarchy();
     
-    // Построение иерархии чиновников (ввод данных от пользователя)
-    void buildHierarchy();
+    void buildFromConsole();
+    void findOptimalBribePath() const;
     
-    // Нахождение решения с минимальной суммой взяток
-    void findMinBribeSolution();
+private:
+    std::unordered_map<int, std::unique_ptr<OfficialNode>> officials;
+    OfficialNode* root = nullptr;
     
-    // Очистка иерархии и освобождение памяти
-    void clearHierarchy();
+    void clear();
+    OfficialNode* findNode(int id) const;
+    void findAllPaths(OfficialNode* node, 
+                     std::vector<int>& currentPath,
+                     std::vector<std::vector<int>>& allPaths,
+                     int currentSum) const;
+    void printResults(const std::vector<int>& bestPath, int minBribe) const;
 };
-
 #endif
