d040f2823057bd941d1d3c523e227317115e1b56
Siempre <71586429+Naymushin-12@users.noreply.github.com>
2025-06-27 05:18:45 +0500

    Update OfficialHierarchy.h

--- OfficialHierarchy.h
+++ OfficialHierarchy.h
@@ -1,10 +1,15 @@
+// OfficialHierarchy.h
 #ifndef OFFICIALHIERARCHY_H
 #define OFFICIALHIERARCHY_H
 
-#include <vector>
-#include <string>
-#include <memory>
-#include <unordered_map>
+// Класс, представляющий узел односвязного списка (для хранения подчиненных)
+class SubordinateNode {
+public:
+    class OfficialNode* data;    // Указатель на чиновника
+    SubordinateNode* next;       // Указатель на следующий узел
+    
+    SubordinateNode(class OfficialNode* official) : data(official), next(nullptr) {}
+};
 
 // Класс, представляющий одного чиновника в иерархии
 class OfficialNode {
@@ -24,13 +29,63 @@ public:
     // Возвращает сумму взятки для этого чиновника
     int getBribe() const;
     
-    // Возвращает список подчиненных (константная ссылка)
-    const std::vector<OfficialNode*>& getSubordinates() const;
+    // Возвращает список подчиненных
+    SubordinateNode* getSubordinates() const;
     
 private:
-    int id;                         // Уникальный идентификатор чиновника
-    int bribeAmount;                // Сумма требуемой взятки
-    std::vector<OfficialNode*> subordinates; // Список подчиненных
+    int id;                     // Уникальный идентификатор чиновника
+    int bribeAmount;            // Сумма требуемой взятки
+    SubordinateNode* subordinatesHead; // Голова списка подчиненных
+};
+
+// Класс, представляющий узел списка для хранения пути
+class PathNode {
+public:
+    int id;                     // ID чиновника в пути
+    PathNode* next;             // Следующий элемент пути
+    
+    PathNode(int id) : id(id), next(nullptr) {}
+};
+
+// Класс, представляющий список путей
+class PathList {
+public:
+    PathNode* head;             // Голова списка
+    PathNode* tail;             // Хвост списка (для быстрого добавления)
+    
+    PathList() : head(nullptr), tail(nullptr) {}
+    ~PathList();
+    
+    void addNode(int id);       // Добавление узла в конец списка
+    PathList* clone() const;    // Создание копии списка
+};
+
+// Класс, представляющий список списков (все возможные пути)
+class PathListCollection {
+public:
+    class PathListNode {
+    public:
+        PathList* data;            // Один путь
+        PathListNode* next;         // Следующий элемент
+        
+        PathListNode(PathList* path) : data(path), next(nullptr) {}
+    };
+    
+    PathListNode* head;             // Голова списка
+    
+    PathListCollection() : head(nullptr) {}
+    ~PathListCollection();
+    
+    void addPath(PathList* path);   // Добавление пути в коллекцию
+};
+
+// Класс, представляющий узел списка чиновников
+class OfficialListNode {
+public:
+    OfficialNode* data;         // Указатель на чиновника
+    OfficialListNode* next;      // Указатель на следующий узел
+    
+    OfficialListNode(OfficialNode* official) : data(official), next(nullptr) {}
 };
 
 // Класс, представляющий всю иерархию чиновников
@@ -57,14 +112,14 @@ private:
     
     // Рекурсивный поиск всех возможных путей подписания документов
     void findAllPaths(OfficialNode* node,
-                    std::vector<int>& currentPath,
-                    std::vector<std::vector<int>>& allPaths,
-                    int currentSum) const;
+                    PathList* currentPath,
+                    PathListCollection& allPaths) const;
     
     // Вывод результатов (минимальной суммы и оптимального пути)
-    void printResults(const std::vector<int>& bestPath, int minBribe) const;
+    void printResults(PathList* bestPath, int minBribe) const;
 
-    std::unordered_map<int, std::unique_ptr<OfficialNode>> officials; // Все чиновники
-    OfficialNode* root = nullptr; // Корень иерархии (главный чиновник)
+    OfficialListNode* officialsHead; // Голова списка всех чиновников
+    OfficialNode* root;             // Корень иерархии (главный чиновник)
 };
+
 #endif
