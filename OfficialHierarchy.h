2f03d2750e723b111d4ad8baed314a3780ccb095
Siempre <71586429+Naymushin-12@users.noreply.github.com>
2025-06-27 05:34:10 +0500

    Update OfficialHierarchy.h

--- OfficialHierarchy.h
+++ OfficialHierarchy.h
@@ -1,125 +1,70 @@
-// OfficialHierarchy.h
 #ifndef OFFICIALHIERARCHY_H
 #define OFFICIALHIERARCHY_H
 
-// Класс, представляющий узел односвязного списка (для хранения подчиненных)
-class SubordinateNode {
-public:
-    class OfficialNode* data;    // Указатель на чиновника
-    SubordinateNode* next;       // Указатель на следующий узел
-    
-    SubordinateNode(class OfficialNode* official) : data(official), next(nullptr) {}
-};
+using namespace std;
 
-// Класс, представляющий одного чиновника в иерархии
+// Узел, представляющий одного чиновника
 class OfficialNode {
 public:
-    // Конструктор: создает чиновника с указанным ID и суммой взятки
     OfficialNode(int id, int bribe);
+    ~OfficialNode();
     
-    // Устанавливает начальника для текущего чиновника
-    void setBoss(OfficialNode* boss);
+    void setBoss(OfficialNode* boss);          // Установить начальника
+    void addRequiredOfficial(OfficialNode* req); // Добавить обязательную подпись
+    void addSubordinate(OfficialNode* sub);    // Добавить подчиненного
     
-    // Добавляет подчиненного к текущему чиновнику
-    void addSubordinate(OfficialNode* subordinate);
-    
-    // Возвращает ID чиновника
-    int getId() const;
-    
-    // Возвращает сумму взятки для этого чиновника
-    int getBribe() const;
-    
-    // Возвращает список подчиненных
-    SubordinateNode* getSubordinates() const;
-    
+    int getId() const;                         // Получить ID
+    int getBribe() const;                      // Получить сумму взятки
+    OfficialNode* getBoss() const;             // Получить начальника
+    OfficialNode* getFirstRequired() const;    // Первый обязательный чиновник
+    OfficialNode* getFirstSubordinate() const; // Первый подчиненный
+    OfficialNode* getNextRequired() const;     // Следующий обязательный
+    OfficialNode* getNextSubordinate() const;  // Следующий подчиненный
+
 private:
-    int id;                     // Уникальный идентификатор чиновника
-    int bribeAmount;            // Сумма требуемой взятки
-    SubordinateNode* subordinatesHead; // Голова списка подчиненных
+    int id;               // Идентификатор чиновника
+    int bribeAmount;      // Сумма взятки
+    OfficialNode* boss;   // Начальник
+    OfficialNode* firstRequired;    // Список обязательных подписей
+    OfficialNode* firstSubordinate; // Список подчиненных
+    OfficialNode* nextRequired;     // Следующий в списке обязательных
+    OfficialNode* nextSubordinate;  // Следующий в списке подчиненных
 };
 
-// Класс, представляющий узел списка для хранения пути
+// Узел для хранения пути
 class PathNode {
 public:
-    int id;                     // ID чиновника в пути
-    PathNode* next;             // Следующий элемент пути
+    PathNode(int id, PathNode* next = nullptr);
+    ~PathNode();
     
-    PathNode(int id) : id(id), next(nullptr) {}
+    int getId() const;            // Получить ID чиновника в пути
+    PathNode* getNext() const;    // Следующий узел пути
+    void setNext(PathNode* next); // Установить следующий узел
+
+private:
+    int id;         // ID чиновника
+    PathNode* next; // Следующий элемент пути
 };
 
-// Класс, представляющий список путей
-class PathList {
-public:
-    PathNode* head;             // Голова списка
-    PathNode* tail;             // Хвост списка (для быстрого добавления)
-    
-    PathList() : head(nullptr), tail(nullptr) {}
-    ~PathList();
-    
-    void addNode(int id);       // Добавление узла в конец списка
-    PathList* clone() const;    // Создание копии списка
-};
-
-// Класс, представляющий список списков (все возможные пути)
-class PathListCollection {
-public:
-    class PathListNode {
-    public:
-        PathList* data;            // Один путь
-        PathListNode* next;         // Следующий элемент
-        
-        PathListNode(PathList* path) : data(path), next(nullptr) {}
-    };
-    
-    PathListNode* head;             // Голова списка
-    
-    PathListCollection() : head(nullptr) {}
-    ~PathListCollection();
-    
-    void addPath(PathList* path);   // Добавление пути в коллекцию
-};
-
-// Класс, представляющий узел списка чиновников
-class OfficialListNode {
-public:
-    OfficialNode* data;         // Указатель на чиновника
-    OfficialListNode* next;      // Указатель на следующий узел
-    
-    OfficialListNode(OfficialNode* official) : data(official), next(nullptr) {}
-};
-
-// Класс, представляющий всю иерархию чиновников
+// Класс иерархии чиновников
 class OfficialHierarchy {
 public:
-    // Конструктор по умолчанию
     OfficialHierarchy();
-    
-    // Деструктор - освобождает ресурсы
     ~OfficialHierarchy();
     
-    // Построение иерархии через консольный ввод
-    void buildFromConsole();
-    
-    // Поиск и вывод оптимального пути с минимальной суммой взяток
-    void findOptimalBribePath() const;
-    
-private:
-    // Очистка иерархии (удаление всех чиновников)
-    void clear();
-    
-    // Поиск чиновника по ID (возвращает nullptr если не найден)
-    OfficialNode* findNode(int id) const;
-    
-    // Рекурсивный поиск всех возможных путей подписания документов
-    void findAllPaths(OfficialNode* node,
-                    PathList* currentPath,
-                    PathListCollection& allPaths) const;
-    
-    // Вывод результатов (минимальной суммы и оптимального пути)
-    void printResults(PathList* bestPath, int minBribe) const;
+    void buildFromConsole();       // Построение иерархии из консоли
+    void findOptimalBribePath() const; // Поиск оптимального пути
 
-    OfficialListNode* officialsHead; // Голова списка всех чиновников
-    OfficialNode* root;             // Корень иерархии (главный чиновник)
+private:
+    void clear();                  // Очистка иерархии
+    OfficialNode* findNode(int id) const; // Поиск чиновника по ID
+    void findAllPaths(OfficialNode* node, PathNode* path, 
+                     int sum, PathNode*& bestPath, int& minSum) const; // Поиск всех путей
+    void printResults(PathNode* path, int sum) const; // Вывод результатов
+    void addRequiredDependencies(); // Добавление обязательных зависимостей
+
+    OfficialNode* root;           // Главный чиновник
+    OfficialNode* firstOfficial;  // Список всех чиновников
 };
 
 #endif
